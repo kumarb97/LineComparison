1148bbd5414ddb525c3253b89bee3743f4f01260
kumarb97 <kumarbamankar@gmail.com>
2022-05-03 22:28:16 +0530

    Ability to check length of lines are equal or not

--- LineComparison.php
+++ LineComparison.php
@@ -1,17 +1,30 @@
 <?php
 
 // creating a class LineComparison
-class LineComparison{
+class LineComparison
+{
+    protected $x1;
+    protected $y1;
+    protected $x2;
+    protected $y2;
+
+    function __construct($x1, $y1, $x2, $y2,)
+    {
+        $this->x1 = $x1;
+        $this->x2 = $x2;
+        $this->y1 = $y1;
+        $this->y2 = $y2;
+    }
 
     /**
      * Creating a function Length of line
      * Parameter - Co-ordinate points of line
      * returns length of line.
      **/
-    function lengthOfLine($x1,$y1,$x2,$y2){
-        $temp1 = pow(($x2 - $x1),2);
-        $temp2 = pow(($y2- $y1),2);
-        return pow(($temp1+$temp2),0.5);
+    function lengthOfLine()
+    {
+        $temp1 = pow(($this->x2 - $this->x1), 2);
+        $temp2 = pow(($this->y2 - $this->y1), 2);
+        return pow(($temp1 + $temp2), 0.5);
     }
 }
-?>
--- Main.php
+++ Main.php
@@ -1,26 +1,42 @@
 <?php
 include "LineComparison.php";
-class Main{
+class Main
+{
 
     //main function
-    function main(){
+    function main()
+    {
+        echo "First Line Details \n";
         $x1 = readline("Enter x1 of point (x1,y1) : ");
         $y1 = readline("Enter y1 of point (x1,y1) : ");
         $x2 = readline("Enter x2 of point (x2,y2) : ");
         $y2 = readline("Enter y2 of point (x2,y2) : ");
+        echo "Second Line Deatils \n";
+        $x3 = readline("Enter x3 of point (x3,y3) : ");
+        $y3 = readline("Enter y3 of point (x3,y3) : ");
+        $x4 = readline("Enter x4 of point (x4,y4) : ");
+        $y4 = readline("Enter y4 of point (x4,y4) : ");
 
         // checking whether the given input is valid or not
-        if(is_numeric($x1) && is_numeric($y1) && is_numeric($x2) && is_numeric($y2)){
-            $linecompare = new LineComparison();
-            echo "Length of line is " . round($linecompare->lengthOfLine($x1,$y1,$x2,$y2),3);
-        }
-        else{
+        if (
+            is_numeric($x1) && is_numeric($y1) && is_numeric($x2) && is_numeric($y2) && is_numeric($x3)
+            && is_numeric($y3) && is_numeric($x4) && is_numeric($y4)
+        ) {
+            $firstLine = new LineComparison($x1, $y1, $x2, $y2);
+            $firstLine->lengthOfLine();
+            $line1 = $firstLine->lengthOfLine();
+            $secondLine = new LineComparison($x3, $y3, $x4, $y4);
+            $line2 = $secondLine->lengthOfLine();
+            if ($line1 == $line2) {
+                echo "Lines are Equal";
+            } else {
+                echo "Lines are not Equal";
+            }
+        } else {
             echo "Please Enter Valid Input";
         }
-
     }
 }
 
 $mainObj = new Main();
 $mainObj->main();
-?>
